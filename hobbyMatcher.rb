4e22d7bb9b9f8a04afba82702fe0fd68f19b30a5
tony-dorfmeister <tony.dorfmeister@me.com>
2017-10-14 17:16:09 +0200

    user input for name and hobbies

--- hobbyMatcher.rb
+++ hobbyMatcher.rb
@@ -1,6 +1,23 @@
+
 class Person
   attr_accessor :name, :hobbies
 
-  def initialize
+  # initialize() == Constructor
+  def initialize()
   end
+
 end
+
+# create a new person
+person = Person.new
+
+# ask for users name
+puts 'Hey there! Please tell me your name:'
+person.name = gets.chomp
+puts 'Hi ' + person.name + '!'
+
+# ask for users hobbies
+puts 'Now tell me your hobbies!'
+hobbies_temp = gets.chomp
+person.hobbies = hobbies_temp.split(', ')
+puts person.hobbies
