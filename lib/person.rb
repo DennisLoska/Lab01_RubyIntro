e7b59a4a755b786eb278cf0a5e20f9c9a7161743
dennis <dennis.loska@firstfinance-gmbh.de>
2017-10-18 16:40:55 +0200

    setting up project for rspec-tests with the spec tests of the lab01-exercise github repository of INFO3

---
+++ lib/person.rb
@@ -0,0 +1,8 @@
+class Person
+  attr_accessor :name, :hobbies
+
+  def initialize
+  end
+  def self.friendslist(persons)
+  end
+end
---
+++ spec/person_init_spec.rb
@@ -0,0 +1,39 @@
+require "rspec"
+require "./lib/person.rb"
+
+describe "Person initialization" do
+
+  # remove the pendings one by one and implement the example
+
+  it "should initialize it's attributes from a hash" do
+    pending
+    attribute_hash =   {:name => "Dagobert", :hobbies => "Money"}
+    p = Person.new(attribute_hash)
+    expect(p.name).to eq "Dagobert"
+    expect(p.hobbies).to eq ["Money"]
+  end
+
+  it "should raise an UnknownAttributeError if an attribute is unknown" do
+    pending
+    argument_hash =   {:name => "Dagobert", :hobbies => "Money",:nonexistent_attribute => "some Value"}
+    expect { p = Person.new(argument_hash) }.to raise_error(UnknownAttributeError)
+  end
+
+  it "should still be initializable without any parameters" do
+    p = Person.new
+  end
+
+  it "should work with an existent arbitrary attribute of Person" do
+    pending
+    random_attribute_name = (0...5).map { (97 + rand(26)).chr }.join
+    RFN = random_attribute_name.to_sym
+    class Person
+      attr_accessor RFN
+    end
+    random_attribute_value = "The Value of the Random attribute"
+    argument_hash =   {:name => "Dagobert", :hobbies => "Money",random_attribute_name.to_sym => random_attribute_value}
+    p = Person.new(argument_hash)
+    expect(p.send(random_attribute_name)).to eq random_attribute_value
+  end
+
+end
