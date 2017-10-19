28872c2116f3104e562b9611c5f5bd335f1013e7
dennis <dennis.loska@firstfinance-gmbh.de>
2017-10-19 02:13:25 +0200

    finishing first user story + .gitignore

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,28 @@ class Person
   attr_accessor :name, :hobbies
 
   def initialize
+    @name = name
+    @hobbies = hobbies
   end
+
   def self.friendslist(persons)
   end
+
+  def hobbies=(hobbies)
+    hobbies.split(',')
+    Array.new.each do |hobby|
+      hobbies.push(hobby)
+    end
+    @hobbies = hobbies
+  end
 end
+
+p = Person.new
+puts "What is your name?"
+p.name = gets.chomp
+puts "What are your hobbies?"
+p.hobbies = gets.chomp
+puts  p.name + " - hobbies:"
+printf p.hobbies
+
+# END of first user story
