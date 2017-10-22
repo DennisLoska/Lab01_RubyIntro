dfa1dd6dd5dcfa9d9c4d5cce965238b8acd89147
tony-dorfmeister <tony.dorfmeister@me.com>
2017-10-22 16:50:32 +0200

    fixed user story 1

--- lib/person.rb
+++ lib/person.rb
@@ -1,29 +1,32 @@
 class Person
+# Fields
   attr_accessor :name, :hobbies
 
-  def initialize
+# Constructor
+  def initialize(hash)
     @name = name
     @hobbies = hobbies
   end
 
-  def self.friendslist(persons)
-  end
+def self.friendslist(persons)
+end
 
+# Class Methods
   def hobbies=(hobbies)
-    hobbies.split(',')
-    Array.new.each do |hobby|
-      hobbies.push(hobby)
-    end
-    @hobbies = hobbies
+    hobbies.split(",")
+    @hobbies = hobbies.split(",")
   end
 end
 
-p = Person.new
+
+# Gedöns 
+p = Person.new(name: "Donald", hobbies: "Money, Bathing")
+
 puts "What is your name?"
 p.name = gets.chomp
 puts "What are your hobbies?"
 p.hobbies = gets.chomp
 puts  p.name + " - hobbies:"
-printf p.hobbies
+puts p.hobbies
 
 # END of first user story
